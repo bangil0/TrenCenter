583bf876274053819e5ce9b6a5a97cdc3a4e44bd
andreepratama27 <andreepratama27@gmail.com>
2018-09-04 16:07:18 +0700

    Half Done

---
+++ app/src/main/java/com/meivaldi/trencenter/activity/pendukung/Pendukung.java
@@ -0,0 +1,15 @@
+package com.meivaldi.trencenter.activity.pendukung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.meivaldi.trencenter.R;
+
+public class Pendukung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendukung);
+    }
+}
