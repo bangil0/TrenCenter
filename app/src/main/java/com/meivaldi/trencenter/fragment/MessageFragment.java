c6439fe1c3831ac69993e1173604510f3468f3f7
andreepratama27 <andreepratama27@gmail.com>
2018-09-15 10:36:29 +0700

    Nothing

--- app/src/main/java/com/meivaldi/trencenter/fragment/MessageFragment.java
+++ app/src/main/java/com/meivaldi/trencenter/fragment/MessageFragment.java
@@ -1,21 +1,16 @@
 package com.meivaldi.trencenter.fragment;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.meivaldi.trencenter.R;
 import com.meivaldi.trencenter.activity.KirimPesan;
