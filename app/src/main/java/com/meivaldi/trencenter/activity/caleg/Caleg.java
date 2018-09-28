d6ddfde862a05be8f1be269d0e00932eeb5da4c9
andreepratama27 <andreepratama27@gmail.com>
2018-09-28 11:13:51 +0700

    Nothing

---
+++ app/src/main/java/com/meivaldi/trencenter/activity/caleg/Caleg.java
@@ -0,0 +1,15 @@
+package com.meivaldi.trencenter.activity.caleg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.meivaldi.trencenter.R;
+
+public class Caleg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caleg);
+    }
+}
