390c2d11d16312ba9bb8cf2cffe56b55beb116a2
andreepratama27 <andreepratama27@gmail.com>
2018-11-16 17:32:18 +0700

    Nothing

--- app/src/main/java/com/bmc/trencenter/activity/VisiMisiAdmin.java
+++ app/src/main/java/com/bmc/trencenter/activity/VisiMisiAdmin.java
@@ -1,10 +1,13 @@
 package com.bmc.trencenter.activity;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.bmc.trencenter.R;
@@ -60,9 +63,26 @@ public class VisiMisiAdmin extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.visi_misi, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.tambah_visi_misi){
+            Intent intent = new Intent(getApplicationContext(), TambahMisi.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
     }
 
     @Override
